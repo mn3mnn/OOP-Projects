0b23ecdaa6a25e094dfa19d3d5ba51bd866077ca
EzzEl-Deen <ezzasdezz123@gmail.com>
2022-10-09 20:08:59 +0200

    modify some errors in operator + Size and Sign Function

--- BigDecimalInt.cpp
+++ BigDecimalInt.cpp
@@ -1,51 +1,4 @@
-//
-// Created by DELL on 10/4/2022.
-//
-
 #include "BigDecimalInt.h"
-
-void BigDecimalInt::num1PlusNum2(BigDecimalInt &anotherDec){
-
-    int j = digits.size() - 1;
-    for (size_t i = anotherDec.digits.size() - 1; i >= 0 && j >= 0; i--, j--)
-    {
-        // tmp = the sum of the two numbers
-        int tmp = (anotherDec.digits[i]-'0') + (digits[j]-'0');
-
-        // store last digit of tmp in the longest number
-        anotherDec.digits[i] = ((tmp % 10) + '0');
-
-        if(tmp > 9){
-            // add one to the prev digit
-            anotherDec.digits[i-1] = ((anotherDec.digits[i-1]-'0') + 1) + '0';
-        }
-    }
-}
-
-// in case that i want to make number1 - number2 (number1 is the bigger )
-void BigDecimalInt::num1MinusNum2(BigDecimalInt &anotherDec){
-
-    int j = digits.size() - 1;
-    for (size_t i = anotherDec.digits.size() - 1; i >= 0 && j >=0; i--, j--)
-    {
-        // tmp = difference between the two digits
-        int tmp = (anotherDec.digits[i]-'0') - (digits[j]-'0');
-
-        // if the difference is positive
-        if (tmp < 0){
-            // borrow one from the prev digit
-            anotherDec.digits[i - 1] = ((anotherDec.digits[i - 1]-'0') - 1) + '0';
-            // tmp2 = 10 + the small digit
-            int tmp2 = ((anotherDec.digits[i]-'0') + 10);
-            // store the difference in curr digit in the longest number
-            anotherDec.digits[i] = (tmp2 - (digits[j]-'0')) + '0';
-        }
-        else{
-            // if the difference is positive
-            anotherDec.digits[i] = (tmp + '0');
-        }
-    }
-}
 
 string BigDecimalInt::validate_str(string str1){
     if (!(str1[0] == '+' || str1[0] == '-' || (str1[0] < '9' && str1[0] > '0'))){
@@ -66,44 +19,38 @@ string BigDecimalInt::validate_str(string str1){
 
 
 BigDecimalInt BigDecimalInt::operator+(BigDecimalInt anotherDec){
-        
+    string tmp_dig = digits;
+
     // anotherDec.digits = the longest number
-    if (digits.size() > anotherDec.digits.size()){
-        swap(digits,anotherDec.digits);
+    if (tmp_dig.size() > anotherDec.digits.size()){
+        tmp_dig.swap(anotherDec.digits);
     }
-        
-    // add two positive numbers
-    if(signPositive(anotherDec.digits) && signPositive(digits)){
-        num1PlusNum2(anotherDec);
-    }
-    // add two negative numbers
-    else if (!signPositive(anotherDec.digits)  && !signPositive(digits)){
-        digits.erase(0,1);
-        anotherDec.digits.erase(0,1);
-        anotherDec.digits.insert(0,"-");
-        num1PlusNum2(anotherDec);
-    }
-    // add positive number to negative number (positive number is the smaller) 
-    else if (!signPositive(anotherDec.digits)  && signPositive(digits)){
-        anotherDec.digits.erase(0,1);
-        anotherDec.digits.insert(0,"-");
-        num1MinusNum2(anotherDec);
-    }
-    // add postive number to negative number (positive number is the bigger)
-    else if (signPositive(anotherDec.digits)  && !signPositive(digits)){
-        digits.erase(0,1);
-        num1MinusNum2(anotherDec);
+
+    int j = tmp_dig.size() - 1;
+    for (size_t i = anotherDec.digits.size() - 1; i >= 0 && j >= 0; i--, j--)
+    {
+        // tmp = the sum of the two numbers
+        int tmp = (anotherDec.digits[i]-'0') + (tmp_dig[j]-'0');
+
+        // store last digit of tmp in the longest number
+        anotherDec.digits[i] = ((tmp % 10) + '0');
+
+        if(tmp > 9){
+            // add one to the prev digit
+            anotherDec.digits[i-1] = ((anotherDec.digits[i-1]-'0') + 1) + '0';
+        } 
     }
     // return the longest number
-    return anotherDec;  
+    return anotherDec;
 }
 
 
+
 BigDecimalInt BigDecimalInt::operator- (BigDecimalInt anotherDec){
+
     // anotherDec.digits = the longest number
-    if (digits.size() > anotherDec.digits.size())
-    {
-        swap(digits,anotherDec.digits);
+    if (digits.size() > anotherDec.digits.size()){
+       digits.swap(anotherDec.digits);
     }
 
     int j = digits.size() - 1;
@@ -114,8 +61,17 @@ BigDecimalInt BigDecimalInt::operator- (BigDecimalInt anotherDec){
 
         // if the difference is positive
         if (tmp < 0){
+
+            int k = i - 1;
+            while (anotherDec.digits[k] == '0'){
+                // if we borrow 1 from zero, make it 9 and borrow 1 from the prev digit of zero
+                anotherDec.digits[k] = (anotherDec.digits[k] == '0' ? '9' : anotherDec.digits[k]--);
+                k--;
+            }
             // borrow one from the prev digit
-            anotherDec.digits[i - 1] = ((anotherDec.digits[i - 1]-'0') - 1) + '0';
+            anotherDec.digits[k] = ((anotherDec.digits[k]-'0') - 1) + '0';
+
+
             // tmp2 = 10 + the small digit
             int tmp2 = ((anotherDec.digits[i]-'0') + 10);
             // store the difference in curr digit in the longest number
@@ -134,78 +90,31 @@ BigDecimalInt BigDecimalInt::operator= (BigDecimalInt anotherDec){
     return anotherDec;
 }
 
-//not done (- and + case)
+
 bool BigDecimalInt::operator< (BigDecimalInt anotherDec){
-	if (digits.size() < anotherDec.digits.size()) {
-        return true;
-    }
-	else if (digits.size() == anotherDec.digits.size()) {
-        int COMP = digits.compare(anotherDec.digits);
-        if (COMP == 0) {
-            return false;   //strings are uqual
-        }
-	    else if (COMP < 0) {
-            return true;   
-        }
-		else if (COMP > 0){
-            return false;   //string 1 is bigger
-        }
-	}	
-    // else if () {
-	// 	"for - and +"
-	// }
-    return false;
+    return true;
 }
 
-//not done (- and + case)
+
 bool BigDecimalInt::operator> (BigDecimalInt anotherDec){
-	if (digits.size() > anotherDec.digits.size()) {
-        return true;
-    }
-	else if (digits.size() == anotherDec.digits.size()) {
-        int COMP = digits.compare(anotherDec.digits);
-        if (COMP == 0) {
-            return false;   //strings are uqual
-        }
-	    else if (COMP < 0) {
-            return false;   //string 1 is smaller
-        }
-		else if (COMP > 0){
-            return true;   
-        }
-	}	
-    // else if () {
-	// 	"for - and +"
-	// }
-    return false;
+    return true;
 }
 
 
 bool BigDecimalInt::operator== (BigDecimalInt anotherDec){
-    if (strcmp(anotherDec.digits.c_str(), digits.c_str()) == 0) {
-        return true;
-    }
-	else {
-		return false;
-	}
-    return false;
+    return true;
 }
 
 
-int BigDecimalInt::size() {
-    return digits.size();
+int BigDecimalInt::size(BigDecimalInt test) {
+    string str_tmp = test.digits;
+    return str_tmp.size();
+    
 }
 
 
-bool BigDecimalInt::signPositive(string temp_str){
-    if(temp_str[0] == '-'){return false;}
+bool BigDecimalInt::signPositive(BigDecimalInt testForSign){
+    string str_temp = testForSign.digits;
+    if(str_temp[0] == '-'){return false;}
     else {return true;}
 }
-
-ostream& operator<< (ostream& out, BigDecimalInt num){
-    out << num.digits;
-    return out;
-}
-
-
-
--- BigDecimalInt.h
+++ BigDecimalInt.h
@@ -5,9 +5,7 @@
 #ifndef A1_TASK1_BIGDECIMALINT_H
 #define A1_TASK1_BIGDECIMALINT_H
 #include <string>
-#include <cstring>
-#include <bits/stdc++.h>
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -17,17 +15,13 @@ class BigDecimalInt {
 
 public:
     // constructors
-    BigDecimalInt(string& decStr){
+    BigDecimalInt(string decStr){
         digits = validate_str(decStr);
     }
-    BigDecimalInt(int& decInt) : digits(to_string(decInt)) {}
+    BigDecimalInt(long long decInt) : digits(to_string(decInt)) {}
 
     // take valid bigInt string
     string validate_str(string str1);
-
-    // function cases of operator+
-    void num1PlusNum2(BigDecimalInt &anotherDec);
-    void num1MinusNum2(BigDecimalInt &anotherDec);
 
     // operators overloading
     BigDecimalInt operator+ (BigDecimalInt anotherDec);
@@ -38,10 +32,13 @@ public:
     bool operator> (BigDecimalInt anotherDec);
     bool operator==(BigDecimalInt anotherDec);
 
-    int size();
-    bool signPositive(string temp_str);
+    int size(BigDecimalInt test);
+    bool signPositive(BigDecimalInt testForSign);
 
-   friend ostream& operator<< (ostream& out, BigDecimalInt num);
+   friend ostream& operator<< (ostream& out, BigDecimalInt num){
+        out << num.digits;
+        return out;
+    }
 
 };
 
